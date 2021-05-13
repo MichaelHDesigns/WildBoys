0427e7f179b8a5f3a58cf149510ce456a7d5a029
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2021-05-12 21:36:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -125,75 +125,20 @@ jQuery(document).ready(function ($) {
 
 
 // custom
-(function($) {
-
-  $.fn.menumaker = function(options) {
-      
-      var cssmenu = $(this), settings = $.extend({
-        format: "dropdown",
-        sticky: false
-      }, options);
-
-      return this.each(function() {
-
-        $(this).find(".button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
-            mainmenu.slideToggle().removeClass('open');
-          }
-          else {
-            mainmenu.slideToggle().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
-        });
-
-        cssmenu.find('li ul').parent().addClass('has-sub');
-
-        multiTg = function() {
-          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-          cssmenu.find('.submenu-button').on('click', function() {
-            $(this).toggleClass('submenu-opened');
-            if ($(this).siblings('ul').hasClass('open')) {
-              $(this).siblings('ul').removeClass('open').slideToggle();
-            }
-            else {
-              $(this).siblings('ul').addClass('open').slideToggle();
-            }
-          });
-        };
-
-        if (settings.format === 'multitoggle') multiTg();
-        else cssmenu.addClass('dropdown');
-
-        if (settings.sticky === true) cssmenu.css('position', 'fixed');
-
-        resizeFix = function() {
-          if ($( window ).width() > 468) {
-            cssmenu.find('ul').show();
-
-          }
-
-          if ($(window).width() <= 468) {
-            cssmenu.find('ul').hide().removeClass('open');
-
-          }
-        };
-        resizeFix();
-        return $(window).on('resize', resizeFix);
-
-      });
-  };
-})(jQuery);
-
-(function($){
-$(document).ready(function(){
-$("#cssmenu").menumaker({
-   format: "multitoggle"
+$(document).ready(function(){ 
+	var touch 	= $('#resp-menu');
+	var menu 	= $('.menu');
+ 
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+	
 });
-
-});
-})(jQuery);
-
